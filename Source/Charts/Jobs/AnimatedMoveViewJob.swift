de4cb9bf197e30967c9c4017fd9305344ac5a3fd
Kenrik March <kenrikmarch@icloud.com>
2021-01-20 22:12:12 -0600

    test

--- Source/Charts/Jobs/AnimatedMoveViewJob.swift
+++ Source/Charts/Jobs/AnimatedMoveViewJob.swift
@@ -16,12 +16,12 @@ open class AnimatedMoveViewJob: AnimatedViewPortJob
 {
     internal override func animationUpdate()
     {
-        var pt = CGPoint(
-            x: xOrigin + (CGFloat(xValue) - xOrigin) * phase,
-            y: yOrigin + (CGFloat(yValue) - yOrigin) * phase
-        )
-        
-        transformer.pointValueToPixel(&pt)
-        viewPortHandler.centerViewPort(pt: pt, chart: view)
+//        var pt = CGPoint(
+//            x: xOrigin + (CGFloat(xValue) - xOrigin) * phase,
+//            y: yOrigin + (CGFloat(yValue) - yOrigin) * phase
+//        )
+//        
+//        transformer.pointValueToPixel(&pt)
+//        viewPortHandler.centerViewPort(pt: pt, chart: view)
     }
 }
